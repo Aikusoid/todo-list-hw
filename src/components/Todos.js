8ef58bdec0cd33968f0f87f6edf1efe307d447d9
Aikusoid <Aikusoid@users.noreply.github.com>
2021-03-04 23:12:38 +0100

    Update Todos

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,19 +1,11 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
-export const Todos = ({ todos }) => {
+export const Todos = ({ todos, onToggle }) => {
   return (
     <ul className="list-group">
       {todos.map((todo) => (
-        <li className="list-group-item" key={todo.id}>
-          <input className="form-check-input me-1" type="checkbox" value="" />
-          <div>
-            <strong className="todo-title">{todo.title}</strong>
-            <small>{new Date().toLocaleDateString()}</small>
-          </div>
-          <button className="remover">
-            <i className="fas fa-trash-alt"></i>
-          </button>
-        </li>
+        <TodoItem todo={todo} onChange={onToggle} key={todo.id} />
       ))}
     </ul>
   );
