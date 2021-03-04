e66ebd0c711f4139f1d71775d34020cffd2a0536
Aikusoid <Aikusoid@users.noreply.github.com>
2021-03-04 23:14:26 +0100

    Create Context

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { AddTodo } from "../components/AddTodo";
 import { Todos } from "../components/Todos";
+import Context from "../context/context";
 
 export default function Home() {
-  const todos = new Array(3)
-    .fill("")
-    .map((_, i) => ({ id: i, title: `Note ${i + 1}` }));
+  const [todos, setTodos] = useState(
+    new Array(3)
+      .fill("")
+      .map((_, i) => ({ id: i, title: `Note ${i + 1}`, completed: false }))
+  );
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  }
+
+  function addTodo(title, i) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false,
+        },
+      ])
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
 
   return (
-    <React.Fragment>
-      <AddTodo />
-
-      <Todos todos={todos} />
-    </React.Fragment>
+    <Context.Provider value={{ removeTodo }}>
+      <AddTodo onCreate={addTodo} />
+      {todos.length ? (
+        <Todos todos={todos} onToggle={toggleTodo} />
+      ) : (
+        <h3>Úkoly jsou splněné! 😊</h3>
+      )}
+    </Context.Provider>
   );
 }
