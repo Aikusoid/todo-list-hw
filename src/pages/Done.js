d46d9e5d1985d0d1ce2837b3dd715ffc209f668e
Aikusoid <Aikusoid@users.noreply.github.com>
2021-03-05 12:11:34 +0100

    List of completed todos added

--- src/pages/Done.js
+++ src/pages/Done.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import Context from "../context/context";
 
-export default function Done() {
-  return <h1>What I've alredy done</h1>;
+export default function Done({ todos, toggle }) {
+  const { removeTodo } = useContext(Context);
+
+  return (
+    <React.Fragment>
+      <h3>Seznam splněných úkolů</h3>
+      <ul className="list-group">
+        {todos.map((todo) => {
+          if (todo.completed) {
+            return (
+              <li className="list-group-item">
+                <strong className="todo-title">{todo.title}</strong>
+                <small>{new Date().toLocaleDateString()}</small>
+                <button className="remover" onClick={() => removeTodo(todo.id)}>
+                  <i className="fas fa-trash-alt"></i>
+                </button>
+              </li>
+            );
+          }
+        })}
+      </ul>
+    </React.Fragment>
+  );
 }
