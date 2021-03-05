e98c8b221a05b4143e3b0d15907cbbd8b7bef4fc
Aikusoid <Aikusoid@users.noreply.github.com>
2021-03-05 12:53:56 +0100

    merging components TodoItem & Todos

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,12 +1,30 @@
-import React from "react";
-import TodoItem from "./TodoItem";
+import React, { useContext } from "react";
+// import TodoItem from "./TodoItem";
+import Context from "../context/context";
 
-export const Todos = ({ todos, onToggle }) => {
+export const Todos = ({ todos, onChange }) => {
+  const { removeTodo } = useContext(Context);
   return (
     <ul className="list-group">
-      {todos.map((todo) => (
-        <TodoItem todo={todo} onChange={onToggle} key={todo.id} />
-      ))}
+      {todos.map((todo) => {
+        if (!todo.completed) {
+          return (
+            <li className="list-group-item">
+              <input
+                className="form-check-input me-1"
+                type="checkbox"
+                value=""
+                onChange={() => onChange(todo.id)}
+              />
+              <strong className="todo-title">{todo.title}</strong>
+              <small>{new Date().toLocaleDateString()}</small>
+              <button className="remover" onClick={() => removeTodo(todo.id)}>
+                <i className="fas fa-trash-alt"></i>
+              </button>
+            </li>
+          );
+        }
+      })}
     </ul>
   );
 };
