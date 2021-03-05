61d70e99c0ca211071223833c40068d3f4631707
Aikusoid <Aikusoid@users.noreply.github.com>
2021-03-05 12:10:49 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { AddTodo } from "../components/AddTodo";
 import { Todos } from "../components/Todos";
-import Context from "../context/context";
 
-export default function Home() {
-  const [todos, setTodos] = useState(
-    new Array(3)
-      .fill("")
-      .map((_, i) => ({ id: i, title: `Note ${i + 1}`, completed: false }))
-  );
-
-  function toggleTodo(id) {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed;
-        }
-        return todo;
-      })
-    );
-  }
-
-  function addTodo(title, i) {
+export default function Home({ todos, setTodos, toggleTodo }) {
+  function addTodo(title) {
     setTodos(
       todos.concat([
         {
@@ -33,18 +15,14 @@ export default function Home() {
     );
   }
 
-  function removeTodo(id) {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  }
-
   return (
-    <Context.Provider value={{ removeTodo }}>
+    <React.Fragment>
       <AddTodo onCreate={addTodo} />
       {todos.length ? (
         <Todos todos={todos} onToggle={toggleTodo} />
       ) : (
         <h3>Úkoly jsou splněné! 😊</h3>
       )}
-    </Context.Provider>
+    </React.Fragment>
   );
 }
